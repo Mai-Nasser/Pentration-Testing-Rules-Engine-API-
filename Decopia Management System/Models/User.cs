10d70d2b0b2fb99ffc0d24b8df36b5f4c56a5cc3
Mai Nasser <153108190+Mai-Nasser@users.noreply.github.com>
2026-04-10 18:55:07 +0200

    Update User.cs

--- Decopia Management System/Models/User.cs
+++ Decopia Management System/Models/User.cs
@@ -2,19 +2,17 @@
 {
     public class User
     {
-        public int Id { get; set; }                 // Primary Key
-        public Guid PublicId { get; set; } = Guid.NewGuid(); // PublicId مخفي لليوزر
+        public int Id { get; set; }                  
+        public Guid PublicId { get; set; } = Guid.NewGuid(); 
         public string FullName { get; set; }
         public string Email { get; set; }
-        public string PasswordHash { get; set; }    // نخزن الـ password مش text
-        public string Role { get; set; }            // Admin / User
-        public bool IsActive { get; set; } = true;  // لتفعيل/تعطيل الحساب
-
-        public bool IsDeleted { get; set; } = false;// حذف الحساب
+        public string PasswordHash { get; set; }     
+        public string Role { get; set; }            
+        public bool IsActive { get; set; } = true;   
+        public bool IsDeleted { get; set; } = false; 
         public DateTime? DeletedAt { get; set; }
 
-        // لخاصية Forgot/Reset Password
-        public string? ResetCode { get; set; }
+         public string? ResetCode { get; set; }
         public DateTime? ResetCodeExpiry { get; set; }
     }
 }
